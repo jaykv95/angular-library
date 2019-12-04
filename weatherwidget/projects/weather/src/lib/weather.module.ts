b8e7a151ad65a144f39bed2d0b9eee6bc89d32a3
jaykv95 <jaykv95@gmail.com>
2019-12-04 14:06:12 +0530

    first commit

---
+++ weatherwidget/projects/weather/src/lib/weather.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { WeatherComponent } from './weather.component';
+
+
+
+@NgModule({
+  declarations: [WeatherComponent],
+  imports: [
+  ],
+  exports: [WeatherComponent]
+})
+export class WeatherModule { }
---
+++ weatherwidget/projects/weathertest/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {WeatherModule} from 'weather'
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,WeatherModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
